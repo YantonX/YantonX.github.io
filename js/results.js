2bda06554516c540780a5769071402573bb0bde6
Yanton92i <yaniss.benyagoub@gmail.com>
2023-04-12 18:28:43 -0800

    Update results.js

--- js/results.js
+++ js/results.js
@@ -4,14 +4,27 @@ const resultsContainer = document.querySelector(".section_results");
 async function fetchTokens(searchTerm) {
   try {
     const response = await fetch(
-      `https://api.coingecko.com/api/v3/search?query=${encodeURIComponent(searchTerm)}`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
     );
     const data = await response.json();
-    return data.coins;
+    return data;
   } catch (err) {
     console.error(err);
     return [];
   }
+}
+
+function filterTokens(tokens, keyword) {
+  const currentTime = new Date().getTime();
+  const oneWeek = 7 * 24 * 60 * 60 * 1000; // milliseconds in a week
+
+  const filteredTokens = tokens.filter((token) => {
+    const tokenName = token.name.toLowerCase();
+    const tokenAge = currentTime - new Date(token.ath_date).getTime();
+    return tokenName.includes(keyword.toLowerCase()) && tokenAge <= oneWeek;
+  });
+
+  return filteredTokens;
 }
 
 function displayResults(tokens) {
@@ -44,7 +57,8 @@ async function initResultsPage() {
   }
 
   const tokens = await fetchTokens(searchTerm);
-  displayResults(tokens);
+  const filteredTokens = filterTokens(tokens, searchTerm);
+  displayResults(filteredTokens);
 }
 
 function displayNoSearchTermMessage() {
